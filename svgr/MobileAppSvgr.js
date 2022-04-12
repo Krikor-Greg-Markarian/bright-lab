7df378c1475d746b715dbed04a3bd9eea78386cb
Greg <greg_markarian@hotmail.com>
2022-04-12 15:58:09 +0300

    ADD: mobile app svgr

---
+++ svgr/MobileAppSvgr.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+const MobileAppSvgr = (props) => {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="120"
+        height="165.081"
+        viewBox="0 0 180 165.081"
+      >
+        <g
+          id="Group_716"
+          data-name="Group 716"
+          transform="translate(11104.5 -5056.919)"
+        >
+          <path
+            id="Path_611"
+            data-name="Path 611"
+            d="M0,0H180"
+            transform="translate(-11104.5 5155.5)"
+            fill="none"
+            stroke="#214ee0"
+            stroke-width="2"
+          />
+          <text
+            id="Post_Title_TAG:H5_"
+            data-name="Post Title [TAG:H5]"
+            transform="translate(-11015 5192)"
+            fill="#2d2d40"
+            font-size="25"
+            font-family="CodecPro-Regular, Codec Pro"
+            letter-spacing="0.08em"
+          >
+            <tspan x="-82.516" y="0">
+              Mobile Apps
+            </tspan>
+            <tspan x="0" y="26"></tspan>
+          </text>
+          <g
+            id="noun-mobile-1598018"
+            transform="translate(-11180.352 4989.722)"
+          >
+            <path
+              id="Path_602"
+              data-name="Path 602"
+              d="M266.407,149.351a1.08,1.08,0,0,0,1.081-1.081V68.278a1.08,1.08,0,0,0-1.081-1.081H216.683a1.081,1.081,0,0,0-1.081,1.081v79.991a1.08,1.08,0,0,0,1.081,1.081ZM217.764,69.359h47.562v77.829H217.764ZM244.788,138a3.244,3.244,0,1,1-.95-2.293A3.241,3.241,0,0,1,244.788,138Z"
+              transform="translate(-75.251)"
+              fill="#214ee0"
+            />
+          </g>
+        </g>
+      </svg>
+    </div>
+  );
+};
+
+export default MobileAppSvgr;
