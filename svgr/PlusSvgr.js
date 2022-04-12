951ee871b1b7b2c252cde736c29c67a88d75315b
Greg <greg_markarian@hotmail.com>
2022-04-12 15:58:34 +0300

    ADD: plus svgr blue color

---
+++ svgr/PlusSvgr.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const PlusSvgr = (props) => {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="12.409"
+        height="25.392"
+        viewBox="0 0 42.409 42.392"
+      >
+        <path
+          id="Path_367"
+          data-name="Path 367"
+          d="M857.26,939.47l13.979-.205v-14.2l13.571-.219V939.47H899.45l-.244,13.571H885.013l-.194,13.976-13.58.222v-14.2l-14.2-.016Z"
+          transform="translate(899.45 967.239) rotate(180)"
+          fill="#214ee0"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default PlusSvgr;
