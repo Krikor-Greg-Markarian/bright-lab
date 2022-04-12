f4728923fb3c56c3ca90cefb6175a65b09176fc7
Greg <greg_markarian@hotmail.com>
2022-04-12 15:55:04 +0300

    ADD: font size and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,35 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: '#214EE0',
+        black: '#2D2D40',
+        grey: '#A4B3C6',
+      },
+      fontFamily: {
+        'codec-pro-bold': ['codecProBold'],
+        'codec-pro-bold-italic': ['codecProBoldItalic'],
+        'codec-pro-extra-bold': ['codecProExtraBold'],
+        'codec-pro-extra-bold-italic': ['codecProExtraBoldItalic'],
+        'codec-pro-extra-light-italic': ['codecProExtraLightItalic'],
+        'codec-pro-fat': ['codecProFat'],
+        'codec-pro-italic': ['codecProItalic'],
+        'codec-pro-light': ['codecProLight'],
+        'codec-pro-light-italic': ['codecProLightItalic'],
+        'codec-pro-news': ['codecProNews'],
+        'codec-pro-regular': ['codecProRegular'],
+        'hagrid-text': ['HagridTextVariablesGX'],
+      },
+      fontSize: {
+        30: '1.875rem',
+        60: '3.75rem',
+        25: '1.563rem',
+        58: '3.625rem',
+        20: '1.25rem',
+        24: '1.5rem',
+      },
+    },
   },
   plugins: [],
 };
